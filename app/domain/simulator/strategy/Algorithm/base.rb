874cf1f48145c5496c59e83079c599c0981e3499
Laurent Georget <laurent@lgeorget.eu>
2013-05-24 01:08:47 +0200

    removed unnecesary exception from algorithm

--- app/domain/simulator/strategy/Algorithm/base.rb
+++ app/domain/simulator/strategy/Algorithm/base.rb
@@ -9,7 +9,7 @@ module Simulator
 								def compute_next_dyno_to_use!
 										#this function should not be called if there is no dyno available
 										#because of the most infinite loop that would follow
-										raise "No available dyno" if clients.count { |dyno| dyno.length > 0 } <= 0
+										return nil if clients.count { |dyno| dyno.length > 0 } <= 0
 								end
 						end
 				end
