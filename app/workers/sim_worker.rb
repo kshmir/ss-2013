5871e89a7305897ffde90099b20b5d2ae4c4695b
Cristian Pereyra <criis.pereyra@gmail.com>
2013-06-13 21:02:32 -0300

    fixes some parameters

--- app/workers/sim_worker.rb
+++ app/workers/sim_worker.rb
@@ -3,9 +3,9 @@ module SimWorker
   def self.perform(sim_id)
   	@sim = Simulation.find(sim_id)
 		params = {}
-    params[:input_variables] = { clients_limit: 10,
-                                 max_amount_of_iterations: 100 }
-    algorithm =     Simulator::Strategy::Algorithm::RoundRobinRouting
+    params[:input_variables] = { clients_limit: 20,
+                                 max_amount_of_iterations: 200 }
+    algorithm =     Simulator::Strategy::Algorithm::ShortestQueueRouting
   	load_balancer = Simulator::Strategy::LoadBalancer.with_algorithm algorithm, params
     data = []
   	Simulator::Core.simulate load_balancer do |i, n, stats| data << stats end
