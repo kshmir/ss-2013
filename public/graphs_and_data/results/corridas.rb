592c486d0d4d6ef50d71faed660c7049ac2f225c
Laurent Georget <laurent@lgeorget.eu>
2013-06-26 19:34:36 +0200

    Bugs fixed - now working

--- public/graphs_and_data/results/corridas.rb
+++ public/graphs_and_data/results/corridas.rb
@@ -4,7 +4,7 @@ final_results = []
 20.times do
 	results = {}
 	(30..70).to_a.each do |n|
-		params[:input_variables] = {clients_limit: n, max_amount_of_iterations:n*100};
+		params[:input_variables] = {clients_limit: n, max_amount_of_iterations:n*10};
 		algos = %w(RandomRouting RoundRobinRouting ShortestQueueRouting SmartRouting).map { |name| "Simulator::Strategy::Algorithm::#{name}".constantize }.map { |algo| Simulator::Strategy::LoadBalancer.with_algorithm algo, params };
 
 		results[n] = {}
